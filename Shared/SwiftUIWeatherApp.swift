218866a3aa6daa4af07b5ff30a7c4fda70709c52
Vagelis Agelousis <Vagelis.Agelousis@bankofcyprus.com>
2022-07-27 06:18:03 +0300

    Initial Commit

---
+++ Shared/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  Shared
+//
+//  Created by Vagelis Agelousis on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
